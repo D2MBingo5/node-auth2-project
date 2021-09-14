7ab6829060688355498f49afd383843bfb6e5c45
Leu Family <romanianfobjr@gmail.com>
2021-09-14 17:24:50 -0500

    only middleware function complete

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -25,7 +25,7 @@ const restricted = (req, res, next) => {
   if (err) {
     next({ status: 401, message: 'Token invalid' })
   } else {
-    req.decodedJwt = decodedToken
+    req.decodedToken = decodedToken
     next()
   }
  })
@@ -42,7 +42,11 @@ const only = role_name => (req, res, next) => {
 
     Pull the decoded token from the req object, to avoid verifying it again!
   */
- next()
+ if (role_name === req.decodedToken.role_name) {
+   next()
+ } else {
+   next ({ status: 403, message: 'This is not for you' })
+ }
 }
 
 
